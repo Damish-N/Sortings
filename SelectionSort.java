5b0337409e5408cab3a1a36c6ff3cd8c2c9a4eb2
Damish <damishnisal100@gmail.com>
2020-01-05 12:04:18 +0530

    Selection_one

--- SelectionSort.java
+++ SelectionSort.java
@@ -26,14 +26,14 @@ class SelectionSort
     { 
         int n = arr.length; 
         for (int i=0; i<n; ++i) 
-            System.out.print(arr[i]+" "); 
+            System.out.print(arr11[i]+" "); 
         System.out.println(); 
     } 
   
     // Driver code to test above 
     public static void main(String args[]) 
     { 
-        selectionSort ob = new selectionSort(); 
+        selectionSort ob = new selectionSort123(); 
         int arr[] = {64,25,12,22,11}; 
         ob.sort(arr); 
         System.out.println("Sorted array"); 
